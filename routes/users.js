1928137d57eea2a3accc6e04cd47ae36bd3c4910
Maximilian Moller <maximilian.moller@vintin.de>
2022-03-14 15:40:52 +0100

    krasser typ

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../models");
+const models = db.sequelize.models;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   res.send('respond with a resource');
+  console.log(models);
+  const users = await models.User.findAll();
+  console.log(users)
+
 });
 
 module.exports = router;
